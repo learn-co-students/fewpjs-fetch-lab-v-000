8e14ce9430a87fd70338258c37f39db919f83367
github username <github email address>
2019-11-05 19:57:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
  return fetch('https://anapioficeandfire.com/api/books')
-  .then(response => response.json)
+  .then(response => response.json())
+  .then(json => renderBooks(json))
 }
 
 
