501eb04cc88942b3451ac1207039e3ad972973dd
John Blank <veloblank@gmail.com>
2019-11-06 23:03:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 function fetchBooks() {
-
+	let books = fetch("https://anapioficeandfire.com/api/books")
+		.then(resp => resp.json())
+		.then(json => renderBooks(json));
+	return books;
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+	const main = document.querySelector("main");
+	json.forEach(book => {
+		const h2 = document.createElement("h2");
+		h2.innerHTML = `<h2>${book.name}</h2>`;
+		main.appendChild(h2);
+	});
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+document.addEventListener("DOMContentLoaded", function() {
+	fetchBooks();
+});
