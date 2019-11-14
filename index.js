a2c881e4d5aa94a1248d4a914936ebcd6f20947e
Sushi Chanrai <sushi@pathworks.co>
2019-11-14 15:10:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
+   fetch('https://anapioficeandfire.com/api/books')
    .then(resp => resp.json())
    .then(json => renderBooks(json));
 }
@@ -8,7 +8,7 @@ function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerText = `${book.name}`
+    h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
 }
