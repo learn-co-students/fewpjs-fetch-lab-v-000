e070d104fe3ce69b6a4820b4e32c5af4afb5bfce
Meg Gutshall <meghan.gutshall@gmail.com>
2019-11-19 16:12:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-let books;
-
 function fetchBooks() {
-  fetch("https://anapioficeandfire.com/api/books")
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(json) {
-      books = json;
-    });
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(books => renderBooks(books))
 }
 
-function renderBooks(json) {
+function renderBooks(books) {
   const main = document.querySelector('main')
-  json.forEach(book => {
+  books.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
@@ -21,5 +15,4 @@ function renderBooks(json) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
-  renderBooks(books);
 })
