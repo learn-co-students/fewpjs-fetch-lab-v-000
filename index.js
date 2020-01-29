ce4d8cd3a33ad73551337945109a3c08a8d5c183
William Barela <william.barela.dev@gmail.com>
2020-01-29 06:58:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+ return fetch('https://anapioficeandfire.com/api/books')
+          .then(resp => resp.json())
+          .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
@@ -11,6 +13,4 @@ function renderBooks(json) {
   })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+document.addEventListener('DOMContentLoaded', () => fetchBooks());
