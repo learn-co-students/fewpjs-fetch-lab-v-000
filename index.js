16b75433481601a98035ac2b0834db49642da117
benitolsantos <github email address>
2020-04-20 17:25:10 -0400

    Done.

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ function fetchBooks() {
   return fetch('https://anapioficeandfire.com/api/books') 
   //Using fetch(), we can include requests for data wherever we need to in our code. We can fetch() data on the click of a button or the expansion of an accordion display. There are many older methods for fetching data, but fetch() is the future.
   .then(resp => resp.json())
-  .then(json => console.log(json)); 
+  .then(json => renderBooks(json)); 
   //JavaScript Object Notation is an open standard file format, and data interchange format, that uses human-readable text to store and transmit data objects 
   //json basically takes data files and makes them objects
+  //so, instead of just logging the JSON from the response, we want to call our renderBooks function, and pass the json to it within fetchBooks - Kevin Webster
+  //json is found and given within the same scope of fetchbooks()
+  //because its defined by .then
 }
 
 function renderBooks(json) {
