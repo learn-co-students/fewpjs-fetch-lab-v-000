e0c0ef5d2d7cf2698c89a0c60a1bac80cb143b36
sdcro <sdcrouse91@gmail.com>
2020-04-23 16:25:20 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+          .then(resp => resp.json())
+          .then(json => renderBooks(json));
 
+  // .then(resp => { renderBooks(resp.json()); }); 
+  // This way won't work, as whatever json() returns does not have a forEach method.
 }
+
+/* This is equivalent/better code: 
+  async function fetchBooks() {
+    const resp = await fetch('https://anapioficeandfire.com/api/books');
+    const json = await resp.json();
+    return renderBooks(json);
+  }
+*/
 
 function renderBooks(json) {
   const main = document.querySelector('main')
@@ -14,3 +27,34 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+/* For future reference:
+*
+* To print out the 5th book in the series:
+* 
+* fetch('https://anapioficeandfire.com/api/books/5')
+*   .then(resp => resp.json())
+*   .then(json => console.log(json));
+* 
+* (To get the NAME of that book - "A Feast for Crows" use 'console.log(json["name"])' above. json.name also works.)
+*
+* To print out the 1031st character in the series:
+*
+* fetch('https://anapioficeandfire.com/api/characters/1031')
+*   .then(resp => resp.json())
+*   .then(json => console.log(json));
+*
+* (To get the NAME of that character - Torren Liddle - use 'console.log(json["name"])' above. json.name works, too.)
+*
+* To print out the total number of pages (6626) of all of the books:
+*
+* let totalNumberOfPages = 0;
+*
+* fetch('https://anapioficeandfire.com/api/books')
+*   .then(resp => resp.json())
+*   .then(function(json) {
+*     json.forEach(book => totalNumberOfPages += book["numberOfPages"]);
+*     // book.numberOfPages works, too.
+*     console.log(totalNumberOfPages);
+*   });
+*/
