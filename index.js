f6c167ba6827d3e04410d9586dfc8276a42cb144
dani8439 <dani8439@aol.com>
2020-07-08 12:59:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
   return fetch('https://anapioficeandfire.com/api/books')
     .then(resp => resp.json())
-    .then(json => renderBooks(json))
+    .then(json => renderBooks(json));
+    // .then(json => console.log(json));
 }
 
 function renderBooks(json) {
@@ -11,8 +12,33 @@ function renderBooks(json) {
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+
+
+
+
+
+
+
+
+
+
+//
+// function renderBooks(json) {
+//   const main = document.querySelector('main')
+//   json.forEach(book => {
+//     const h2 = document.createElement('h2')
+//     h2.innerHTML = `<h2>${book.name}</h2>`
+//     main.appendChild(h2)
+//   })
+// }
+//
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks()
+// })
