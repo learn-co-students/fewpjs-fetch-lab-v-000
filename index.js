4dfe3a2f5f6d69d55cb521ccce0a2488b94612a7
Yazgylych Rahymov <y.rahymov@gmail.com>
2020-09-22 14:07:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(json => renderBooks(json))
 }
+
+// function getPosts(){
+//   fetch('https://anapioficeandfire.com/api/books')
+//   .then(res => res.json())
+//   .then(json => renderBooks(json))
+// }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
