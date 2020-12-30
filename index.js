27d94f5e1f50a86c4504452cb8a8ac51bf8ad33d
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-12-30 07:58:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 function fetchBooks() {
-
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((json) => renderBooks(json))
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
+  const main = document.querySelector("main")
+  json.forEach((book) => {
+    const h2 = document.createElement("h2")
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks()
 })
