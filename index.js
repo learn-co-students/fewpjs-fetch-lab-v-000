1dd4cd78d473d30c0dff97ff5b16b44d4e85df94
capricious-slingshot <ephemere@protonmail.com>
2021-03-09 19:03:44 -0800

    Done.

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ function fetchBooks() {
   return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json => renderBooks(json))
-
-
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    let title = document.createElement('h2');
+    title.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
